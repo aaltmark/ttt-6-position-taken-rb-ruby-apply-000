4a4c1069ff8ee2440a16b5f438ddfa5209ba6e45
Adam Altmark <adamaltmark@gmail.com>
2020-04-20 20:49:57 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, index)
   if board[index] != " " || board[index] != ""
     false
-    if board[index] == "X" || board[index] == "O"
+  elsif board[index] == "X" || board[index] == "O"
       true
   end
 end
